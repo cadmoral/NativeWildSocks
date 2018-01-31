b8b7b68c6c1e72d34a90c88e29ed3cb07acc5089
cadmoral <cams_00@hotmail.com>
2018-01-31 16:00:08 +0100

    list vote

--- App.js
+++ App.js
@@ -2,15 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
-import FixedLabel from './app/components/FixedLabel';
-
-import Camera from './app/components/Camera'; 
-import TabTwo from './app/components/TabTwo';
-
-export default TabNavigator({
-   Camera: { screen: Camera }, TabTwo: { screen: TabTwo },
-});
-
+import ListThumbnailExample from './app/components/List';
 
 export default class App extends React.Component {
 
@@ -18,7 +10,7 @@ export default class App extends React.Component {
   render() {
     return (
       
-        <FixedLabel />
+        <ListThumbnailExample />
     
     );
   }
