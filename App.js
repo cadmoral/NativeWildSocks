7d1236c1756690f6e6fe5709ae927b3f0128bf5f
cadmoral <cams_00@hotmail.com>
2018-01-31 16:02:13 +0100

    systeme dossiers

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
-import ListThumbnailExample from './app/components/List';
+import ListThumbnailExample from './components/List';
 
 export default class App extends React.Component {
 
